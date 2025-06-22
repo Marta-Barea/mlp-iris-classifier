b959b705ad917a0276f576b9b756c4854d838b52
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2025-06-22 19:21:17 +0200

    parametrize load_data path

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,17 +1,16 @@
-import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from .config import DATA_PATH, SEED, TEST_SIZE
 
 
-def load_data():
-    iris_data = pd.read_csv(DATA_PATH, skiprows=1, header=None)
+def load_data(path=DATA_PATH):
+    iris_data = pd.read_csv(path, skiprows=1, header=None)
     iris_data = iris_data.drop(columns=[0])
     arr = iris_data.values
 
-    X_iris = arr[:, 0:4].astype(float)
-    y_text = arr[:, 4]
+    X_iris = arr[:, :-1].astype(float)
+    y_text = arr[:, -1]
 
     encoder = LabelEncoder()
     y_iris = encoder.fit_transform(y_text)

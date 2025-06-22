383589aec667c2fa418583ee47f84bd386fbc05b
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2025-06-22 19:21:50 +0200

    pass output dirs to runner

--- run_all.py
+++ run_all.py
@@ -1,20 +1,16 @@
 import sys
 import pathlib
-
 from src.train import train_model
-from src.evaluate import evaluate_model
+from src.evaluate import evaluate_model, get_latest_model
 
 
-def runner():
+def runner(model_dir=None, checkpoint_dir=None, reports_dir=None):
     project_root = pathlib.Path(__file__).parent.parent.resolve()
     sys.path.insert(0, str(project_root))
 
-    models_dir = project_root / "models"
-    models_dir.mkdir(exist_ok=True)
-
     print("=== STARTING TRAINING ===")
     try:
-        train_model()
+        train_model(model_dir=model_dir, checkpoint_dir=checkpoint_dir)
     except Exception as e:
         print("❌ Error during training:")
         print(e)
@@ -22,7 +18,7 @@ def runner():
 
     print("\n=== STARTING EVALUATION ===")
     try:
-        evaluate_model()
+        evaluate_model(output_dir=reports_dir)
     except Exception as e:
         print("❌ Error during evaluation:")
         print(e)
